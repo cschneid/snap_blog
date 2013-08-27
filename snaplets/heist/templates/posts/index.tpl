9492fd23c768d447cfeffbf035ff35d9a8278d5c
Chris Schneider <chris@christopher-schneider.com>
2013-08-27 07:57:07 -0600

    Show a list of posts at /posts
    Uses a <posts> splice, and a <post> splice.
    
    Uses heistLocal to bind the posts splice in the handler, which is
    suboptimal, but works for the moment.

--- snaplets/heist/templates/posts/index.tpl
+++ snaplets/heist/templates/posts/index.tpl
@@ -1,1 +1,9 @@
-Post Index
+<h1>Post Index</h1>
+
+<posts>
+  <post>
+    <postContent />
+  </post>
+
+  <br/>
+</posts>
--- src/Post.hs
+++ src/Post.hs
@@ -5,14 +5,29 @@ module Post where
 import           Data.ByteString (ByteString)
 import           Snap.Snaplet.Heist
 import           Snap.Snaplet
+import qualified Heist.Interpreted as I
+import qualified Data.Text as T
+import qualified Database.Persist as P
+import           Snap.Snaplet.Persistent
 ------------------------------------------------------------------------------
 import Application
-
+import Database
 
 routes :: [(ByteString, Handler App App ())]
 routes = [ ("/posts", Post.postsHandler)
          ]
 
 postsHandler :: Handler App App ()
-postsHandler = render "posts/index"
+postsHandler = do
+  results <- runPersist $ P.selectList [] []
+  let posts = map convert results
+  heistLocal (I.bindSplice "posts" (postsSplice posts)) $ render "posts/index"
+  where 
+    convert (P.Entity _ post) = post
 
+
+postSplice :: Post -> I.Splice (Handler App App)
+postSplice post = I.runChildrenWithText [("postContent", postContent post)]
+
+postsSplice :: [Post] -> I.Splice (Handler App App)
+postsSplice = I.mapSplices postSplice
